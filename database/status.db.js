fc6c1c9e3529b9dd822f5d17903765f4dbb273cc
bartomsilva <bartomsilva@gmail.com>
2023-10-20 15:10:29 -0300

    feat: add status.db contendo os status das tasks

---
+++ database/status.db.js
@@ -0,0 +1,11 @@
+const statusOpen =()=> "aberta"
+const statusPaused =()=> "pausada"
+const statusCancel =()=> "cancelada"
+const statusFinalized =()=> "finalizada"
+
+module.exports = {
+  statusOpen, 
+  statusPaused,
+  statusCancel,
+  statusFinalized
+}
