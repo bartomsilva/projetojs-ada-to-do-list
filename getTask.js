363f498a24ca0955b0f49cb1b6de8ba81ebe0231
Pablo Henrique SC <pabloph0077@gmail.com>
2023-10-21 00:33:39 -0300

    feat: validate id

--- getTask.js
+++ getTask.js
@@ -2,6 +2,12 @@ const tasks = require("./database/tasks");
 
 const getTask = (idTask) => {
   const response = tasks.filter((task) => task.id === idTask);
+
+  if(response.length === 0){
+    console.log('Task não encontrada')
+    return undefined
+  }
+
   return response;
 };
 
